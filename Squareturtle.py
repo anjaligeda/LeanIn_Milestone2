5b218b9e5fa9141e1bc0e34c9a91eae5c3508a6a
Anjali Geda <91786321+anjaligeda@users.noreply.github.com>
2022-02-18 11:42:29 +0530

    Create Squareturtle.py

---
+++ Squareturtle.py
@@ -0,0 +1,41 @@
+
+        #code1
+initializeTurtle()
+speed(10)
+color("black")
+bgcolor("white")
+right(90)
+forward(80)
+right(90)
+forward(80)
+right(90)
+forward(110)
+right(90)
+forward(110)
+right(90)
+forward(140)
+right(90)
+forward(140)
+right(90)
+forward(170)
+right(90)
+forward(170)
+right(90)
+forward(200)
+right(90)
+forward(200)
+right(90)
+
+
+      #code2
+initializeTurtle()
+speed(10)
+color("black")
+bgcolor("white")
+length=80
+for i in range(5):
+  for j in range(2):
+   right(90)
+   forward(length)
+   length=length+30 
+right(90)
