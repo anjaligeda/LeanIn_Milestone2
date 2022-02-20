ed3abfe68b0de8963e9ed134c857990df6161744
Anjali Geda <91786321+anjaligeda@users.noreply.github.com>
2022-02-20 11:30:04 +0530

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -1,7 +1,19 @@
-from mobilechelonian import Turtle
+import turtle
 import math
-tim=Turtle()
-n=int(input())
-for i in range(0,3):
- tim.forward(60)
- tim.left(360/n)
+wn = turtle.Screen()
+wn.bgcolor("white")
+triangle = turtle.Turtle()
+triangle.color("black")
+triangle.shape("turtle")
+triangle.fillcolor("black")
+
+triangle.begin_fill()
+triangle.forward(100) # draw base
+triangle.left(120)
+triangle.forward(100)
+triangle.left(120)
+triangle.forward(100)
+triangle.left(120)
+ 
+
+triangle.end_fill()
